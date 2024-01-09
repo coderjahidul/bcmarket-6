42f26dc5da99c63c76bff6be976fdfc7411cf6f4
coderjahidul <sobuz0349@gmail.com>
2024-01-09 14:44:34 +0600

    woocommerce file version code add and update

--- emails/email-footer.php
+++ emails/email-footer.php
@@ -1,3 +1,22 @@
+<?php
+/**
+ * Email Footer
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-footer.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woo.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 7.4.0
+ */
+
+ defined( 'ABSPATH' ) || exit;
+?>
 <p style="text-align:center">
 	<span style="font-size:15px">______________________________<wbr>____________________</span>
 </p>
