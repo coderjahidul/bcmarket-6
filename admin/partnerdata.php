680825e39535e4eddc6514631c8f869e15ace375
coderjahidul <sobuz0349@gmail.com>
2024-01-27 16:49:42 +0600

    compleat pratner ban function

--- admin/partnerdata.php
+++ admin/partnerdata.php
@@ -145,14 +145,28 @@ get_header(); ?>
 													<div class="deduct_msg"></div>
 												</form>
 											</td>
-											
+											<?php 
+												$current_datetimes = current_time('mysql'); // Get the current datetime in MySQL format
+												$current_datetime = date('Y-m-d H:i', strtotime($current_datetimes));
+
+												// Get the 'account_status_datetime' meta value for the user
+												$account_status_datetimes = get_user_meta($user->ID, 'account_status_datetime', true);
+												$account_status_datetime = str_replace('T', ' ', $account_status_datetimes);
+
+												$ban_timestamp = strtotime($account_status_datetime);
+												$current_timestamp = strtotime($current_datetime);
+												
+												// Compare 'account_status_datetime' with the current datetime
+												if ($ban_timestamp <= $current_timestamp) {
+													// Update 'account_status' to an empty string
+													update_user_meta($user->ID, 'account_status', '');
+													update_user_meta($user->ID, 'account_status_datetime', '');
+												}
+											?>
 											<td>
 												<?php if(get_user_meta($user->ID, 'account_status' , true) == 'rejected') : ?>
 													<button data-id="<?php echo $user->ID; ?>" class="btn  btn-danger ">Banned</button>
 													<button data-id="<?php echo $user->ID; ?>" class="btn  btn-danger rec_partner_account">Re-activate</button>
-													<?php 
-														echo "Ban Time: " . $account_status_datetime . "Current Time: " . $current_datetime;
-													?>
 													
 												<?php else : ?>
 													<input class="ban_inp" type="text" placeholder="Add Ban Reason">
