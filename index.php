5d5c5772b38cf6a5ce540bdd8dfb189567de43ab
coderjahidul <sobuz0349@gmail.com>
2024-02-11 11:05:22 +0600

    blog page and single blog page style css update

--- index.php
+++ index.php
@@ -15,61 +15,6 @@ $day = date_format($post_date_obj, 'd');   // Day without leading zeros
 ?>
 
 
-<style>
-	.post-thumbnail img{
-		width: 100%;
-		height: auto;
-	}
-	.reed-more {
-		background-color: #F6F6F6;
-		border: 1px solid #f0f0f0;
-		display: table;
-		margin-top: 20px;
-		padding: 5px 15px;
-		border-radius: 3px;
-		text-decoration: none !important;
-		font-weight: 500;
-	}
-	.reed-more:hover {
-		background-color: #184675;
-		color: #fff;
-	}
-	.post-content{
-		padding: 30px 20px;
-	} 
-
-	@media (max-width: 767px) {
-		.post-thumbnail img{
-			width: 100%;
-			height: auto;
-		}
-
-		
-		.soc-category .flex h2 {
-			font-size: 20px;
-			line-height: 1.1;
-		}
-		.col-lg-1, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-md-1, .col-md-10, .col-md-11, .col-md-12, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-sm-1, .col-sm-10, .col-sm-11, .col-sm-12, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-xs-1, .col-xs-10, .col-xs-11, .col-xs-12, .col-xs-2, .col-xs-3, .col-xs-4, .col-xs-5, .col-xs-6, .col-xs-7, .col-xs-8, .col-xs-9 {
-			padding-right: 0;
-		}
-	}
-	@media (max-width: 320px) {
-		.post-content{
-			padding: 20px 10px;
-		} 
-		.post-thumbnail img{
-			width: 100%;
-			height: auto;
-		}
-		.col-lg-1, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-md-1, .col-md-10, .col-md-11, .col-md-12, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-sm-1, .col-sm-10, .col-sm-11, .col-sm-12, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-xs-1, .col-xs-10, .col-xs-11, .col-xs-12, .col-xs-2, .col-xs-3, .col-xs-4, .col-xs-5, .col-xs-6, .col-xs-7, .col-xs-8, .col-xs-9 {
-			padding-right: 0;
-		}
-		.soc-category .flex h2 {
-			font-size: 16px;
-			line-height: 1.1;
-		}
-	}
-</style>
 
 <section class="soc-category" id="content">
     
