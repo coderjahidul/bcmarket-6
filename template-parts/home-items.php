b7258760ac7839469e1d28b2547ae144ed0706d1
coderjahidul <sobuz0349@gmail.com>
2024-03-09 12:22:35 +0600

    product listing sort by DESC items-price

--- template-parts/home-items.php
+++ template-parts/home-items.php
@@ -56,7 +56,7 @@
 
                         $pr_query = new WP_Query(array(
                             'post_type' => 'item', 
-                            'posts_per_page' => 5, 
+                            'posts_per_page' => -1, 
                             
                             
                             'tax_query' => array(
@@ -65,6 +65,9 @@
                                     'terms'    => $child->term_id,
                                 ),
                             ),
+                            'meta_key' => 'item_price',
+                            'orderby' => 'meta_value_num',
+                            'order' => 'DESC',
                         ));
 
                         $new_query = new WP_Query(array(
@@ -77,6 +80,9 @@
                                     'terms'    => $child->term_id,
                                 ),
                             ),
+                            'meta_key' => 'item_price',
+                            'orderby' => 'meta_value_num',
+                            'order' => 'DESC',
                         ));
                         // echo "<pre>";
                         // print_r($new_query);
@@ -94,16 +100,160 @@
                             <div class="socs ">
                                 <div class="first_div">
                                 <?php 
-                                   while($pr_query->have_posts()) : $pr_query->the_post();
-                                       get_template_part('template-parts/content', 'item');
-                                   endwhile; wp_reset_postdata();
+                                $count = 0;
+                                while ($pr_query->have_posts() && $count < 5) : $pr_query->the_post();
+                                    $item_id = get_the_ID();
+                                    $total_pcs = get_total_pcs_by_item($item_id);
+                                    $post_thumbnail = get_the_post_thumbnail($item_id, 'thumbnail');
+                                    $post_title = get_the_title($item_id);
+                                    $post_link = get_the_permalink($item_id);
+                                   
+                                    if ($total_pcs != 0) {?>  
+                                        <div class="soc-body">
+                                        <div class="soc-img">
+                                            <a href="<?php echo $post_link; ?>">
+                                                <?php echo $post_thumbnail; ?>
+                                            </a>
+                                        </div>
+                                        <div class="soc-text">
+                                            <p>
+                                                <a href="<?php echo $post_link; ?>"><?php echo $post_title; ?></a>
+                                            </p>
+                                            <a href="<?php echo $post_link; ?>" class="learn-more">More...</a>
+                                        </div>
+                                        <div class="soc-price">
+                                            <p><?php echo get_total_pcs_by_item($item_id); ?> pcs.
+                                                <br>
+                                                <span>Price per pc<br></span>
+                                                <div class="">from $<?php 
+                                            //  $soldoutPrice =  get_field('item_price');
+                                            $price = get_post_meta($item_id, 'item_price', true);
+                                            
+                                            if($price == " "){
+                                            echo get_per_pcs_by_item($item_id);
+                                            }else{
+                                                echo $price;
+                                            }
+                                            ?></div>
+                                            </p>
+                                        </div>
+                                        <div class="soc-qty">
+                                        <?php 
+                                            echo get_total_pcs_by_item($item_id);
+                                        ?> pcs.</div>
+                                        <div class="soc-cost-label">Price per pc</div>
+                                        <div class="soc-cost">from $
+                                        <?php 
+                                            //  $soldoutPrice =  get_field('item_price');
+                                            $price = get_post_meta($item_id, 'item_price', true);
+                                            
+                                            if($price == " "){
+                                            echo get_per_pcs_by_item($item_id);
+                                            }else{
+                                                echo $price;
+                                            }
+                                            ?>
+                                        </div>
+                                        <?php if(get_total_pcs_by_item($item_id) != 0) : ?>
+                                        <div class="soc-cell">
+                                            <button type="button" class="basket-button" data-id="<?php echo $item_id; ?>">
+                                                <img src="<?php echo get_template_directory_uri(); ?>/img/ic-basket.png" alt=""><span>Buy</span>
+                                            </button>
+                                        </div>
+                                    <?php else : ?>
+                                        <div class="subscribe-cell" data-help="Subscribe to newsletter">
+                                            <button class="subscribe_button" type="button" data-id="<?php echo $item_id; ?>">
+                                                <i class="fa-regular fa-envelope"></i>
+                                            </button>
+                                        </div>
+                                    <?php  endif; ?>
+
+                                    </div><?php
+                                    $count++;
+                                    }
+                                endwhile; wp_reset_postdata();
+
+                                //    while($pr_query->have_posts()) : $pr_query->the_post();
+                                //        get_template_part('template-parts/content', 'item');
+                                //    endwhile; wp_reset_postdata();
                                 ?>
                                 </div>
                             
                                     <div class=" new_div_toggle" style="display:none;">
                                         <?php 
-                                            while($new_query->have_posts()) : $new_query->the_post();
-                                                get_template_part('template-parts/content', 'item');
+                                            // while($new_query->have_posts()) : $new_query->the_post();
+                                            //     get_template_part('template-parts/content', 'item');
+                                            // endwhile; wp_reset_postdata();
+
+                                            while ($new_query->have_posts()) : $new_query->the_post();
+                                                $item_id = get_the_ID();
+                                                $total_pcs = get_total_pcs_by_item($item_id);
+                                                $post_thumbnail = get_the_post_thumbnail($item_id, 'thumbnail');
+                                                $post_title = get_the_title($item_id);
+                                                $post_link = get_the_permalink($item_id);
+                                            
+                                                if ($total_pcs != 0) {?>  
+                                                    <div class="soc-body">
+                                                    <div class="soc-img">
+                                                        <a href="<?php echo $post_link; ?>">
+                                                            <?php echo $post_thumbnail; ?>
+                                                        </a>
+                                                    </div>
+                                                    <div class="soc-text">
+                                                        <p>
+                                                            <a href="<?php echo $post_link; ?>"><?php echo $post_title; ?></a>
+                                                        </p>
+                                                        <a href="<?php echo $post_link; ?>" class="learn-more">More...</a>
+                                                    </div>
+                                                    <div class="soc-price">
+                                                        <p><?php echo get_total_pcs_by_item($item_id); ?> pcs.
+                                                            <br>
+                                                            <span>Price per pc<br></span>
+                                                            <div class="">from $<?php 
+                                                        //  $soldoutPrice =  get_field('item_price');
+                                                        $price = get_post_meta($item_id, 'item_price', true);
+                                                        
+                                                        if($price == " "){
+                                                        echo get_per_pcs_by_item($item_id);
+                                                        }else{
+                                                            echo $price;
+                                                        }
+                                                        ?></div>
+                                                        </p>
+                                                    </div>
+                                                    <div class="soc-qty">
+                                                    <?php 
+                                                        echo get_total_pcs_by_item($item_id);
+                                                    ?> pcs.</div>
+                                                    <div class="soc-cost-label">Price per pc</div>
+                                                    <div class="soc-cost">from $
+                                                    <?php 
+                                                        //  $soldoutPrice =  get_field('item_price');
+                                                        $price = get_post_meta($item_id, 'item_price', true);
+                                                        
+                                                        if($price == " "){
+                                                        echo get_per_pcs_by_item($item_id);
+                                                        }else{
+                                                            echo $price;
+                                                        }
+                                                        ?>
+                                                    </div>
+                                                    <?php if(get_total_pcs_by_item($item_id) != 0) : ?>
+                                                    <div class="soc-cell">
+                                                        <button type="button" class="basket-button" data-id="<?php echo $item_id; ?>">
+                                                            <img src="<?php echo get_template_directory_uri(); ?>/img/ic-basket.png" alt=""><span>Buy</span>
+                                                        </button>
+                                                    </div>
+                                                <?php else : ?>
+                                                    <div class="subscribe-cell" data-help="Subscribe to newsletter">
+                                                        <button class="subscribe_button" type="button" data-id="<?php echo $item_id; ?>">
+                                                            <i class="fa-regular fa-envelope"></i>
+                                                        </button>
+                                                    </div>
+                                                <?php  endif; ?>
+            
+                                                </div><?php
+                                                }
                                             endwhile; wp_reset_postdata();
                                             
                                         ?>
