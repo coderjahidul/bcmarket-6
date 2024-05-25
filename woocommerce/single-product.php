86ab2e8113fd79488a97761cc0451680540599e7
coderjahidul <sobuz0349@gmail.com>
2024-05-25 10:27:41 +0600

    update subscribe popup function all taxonomy and ctg page

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -57,10 +57,36 @@ get_header( 'shop' ); ?>
                                 </a>
                             </div>
                         <?php else : ?>
+                            <?php $item_id = get_the_ID(); ?>
                              <div class="subscribe-cell" data-help="Subscribe to newsletter">
-                                <a class="subscribe_button" style="margin-right:0;background-color:#fff;" type="button" data-id="<?php echo get_the_ID(); ?>">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/ic-subscribe.png" height="18" width="21" alt="">
-                                </a>
+                                <!-- Button trigger modal -->
+                                <button type="button" class="subscribe_button" data-toggle="modal" data-target="#exampleModal<?php echo $item_id; ?>">
+                                    <i class="fa-regular fa-envelope"></i>
+                                </button>
+
+                                <!-- Subscribe Modal -->
+                                <div class="modal fade" id="exampleModal<?php echo $item_id; ?>" tabindex="-1" role="dialog" aria-labelledby="subscribeModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document" style="width: 800px;">
+                                        <div class="modal-content" style="margin-top: 200px;">
+                                            <div class="modal-header" style="display: flex;">
+                                                <h5 class="modal-title" id="subscribeModalLabel" style="font-size: 14px; text-transform: uppercase; line-height: 2.428571;">Subscribe to Newsletter</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="margin-left: 500px !important; font-size: 35px; font-weight: 400; color: #000; margin: 0; border: 0;">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-bodys" style="padding: 25px;">
+                                                <p style="font-size: 14px; font-weight: 400; color: #000;">You want to subscribe to an item update:</p>
+                                                <p style="font-size: 14px; font-weight: 600; color: #000; margin-bottom: 10px !important;">GMail Accounts | Accounts could be used in some services. The accounts are verified through SMS. Phone number not included in Profile Security method. Male or female. Registered from different countries IPs.</>
+                                                <form id="subscribe-form-<?php echo $item_id; ?>" action="<?php echo esc_url(admin_url('admin-ajax.php')); ?>" method="POST">
+                                                    <input type="email" name="subscriber_email" placeholder="Enter Your Email" required>
+                                                    <input type="hidden" name="action" value="subscribe_form">
+                                                    <p style="margin-top: 10px; color: red; font-size: 14px; font-weight: 400;">After clicking "subscribe" you will need to confirm your subscription in the mail!</p>
+                                                    <button type="submit" style="width: 120px; height: 30px; line-height: 1.2; text-align: center; color: #fff; font-size: 16px; margin-top: 10px; margin-left: 0; border-radius: 4px; background-color: #245F9B; border: none;">Subscribe</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         <?php endif; ?>
                          
