a4433148be5fb4db49ab8d76f7ad991bc07738b9
coderjahidul <sobuz0349@gmail.com>
2024-07-14 13:50:42 +0600

    modifed users par page show in partner history

--- partner-history.php
+++ partner-history.php
@@ -40,7 +40,7 @@ function partner_history_page_content() {
         // Use get_query_var as a fallback
         $current_page = max(1, get_query_var('paged', $current_page));
 
-        $users_per_page = 100; // Change this as needed
+        $users_per_page = 10; // Change this as needed
         $offset = ($current_page - 1) * $users_per_page;
 
         $user_query = new WP_User_Query(array(
